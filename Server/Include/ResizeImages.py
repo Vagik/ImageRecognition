0b1ba9de897e4b08ff499e6c881c855cdeb45b10
Vaginak Simonyan <vaginak.simonyan.consultant@nielsen.com>
2020-05-05 22:00:31 +0300

    Added new images and new classes

--- Server/Include/ResizeImages.py
+++ Server/Include/ResizeImages.py
@@ -4,18 +4,21 @@ import cv2
 
 if __name__ == "__main__":
 
-    raw_dir = 'C:/Git/ImageRecognition/Server/Images/train'
+    raw_dir = 'C:/Git/ImageRecognition/Server/Images/to_resize'
     save_dir = 'C:/Git/ImageRecognition/Server/Images/resized'
     ext = 'jpg'
-    target_size = (800, 600)
+    target_width = 900
 
     fnames = glob.glob(os.path.join(raw_dir, "*.{}".format(ext)))
     os.makedirs(save_dir, exist_ok=True)
 
     for index, fname in enumerate(fnames):
-        print(".", end="", flush=True)
+        print(fname + '\n', end="", flush=True)
         img = cv2.imread(fname)
+        height, width, channels = img.shape
+        ratio = float(width) / float(target_width)
+        target_size = (target_width, int(float(height) / ratio))
         img_small = cv2.resize(img, target_size)
-        new_fname = "{}.{}".format(str(index), ext)
+        new_fname = "{}.{}".format(str(index + 158), ext)
         small_fname = os.path.join(save_dir, new_fname)
         cv2.imwrite(small_fname, img_small)
