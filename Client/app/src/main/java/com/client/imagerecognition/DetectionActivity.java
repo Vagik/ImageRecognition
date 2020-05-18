de67b349f1e0bcc17bd1125575c743751590cecd
Vaginak Simonyan <vaginak.simonyan.consultant@nielsen.com>
2020-05-18 22:48:05 +0300

    Added tflite to app

--- Client/app/src/main/java/com/client/imagerecognition/DetectionActivity.java
+++ Client/app/src/main/java/com/client/imagerecognition/DetectionActivity.java
@@ -1,5 +1,12 @@
 package com.client.imagerecognition;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -7,6 +14,8 @@ import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class DetectionActivity extends AppCompatActivity {
 
@@ -14,7 +23,7 @@ public class DetectionActivity extends AppCompatActivity {
 
     private ImageView detectionImageView;
     private String filePath;
-
+    private Classifier detector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +33,26 @@ public class DetectionActivity extends AppCompatActivity {
         detectionImageView = findViewById(R.id.detection_image_view);
 
         Picasso.get().load(new File(filePath)).fit().into(detectionImageView);
+        try {
+            detector = ObjectDetectionModel.create(getAssets(), "model.tflite", "label_map.txt", 300, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 300, 300, false);
+        try{
+            List<Classifier.Recognition> results = detector.recognizeImage(scaledBitmap);
+            int a = 5;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Bitmap bitmapd = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmapd);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(50, 50, 10, paint);
+        detectionImageView.setImageBitmap(bitmapd);
     }
 }
