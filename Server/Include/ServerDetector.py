5b12bc31a15f7b415e68296b53761d7472e2960e
Vaginak Simonyan <vaginak.simonyan.consultant@nielsen.com>
2020-05-20 18:26:28 +0300

    Added model files

--- Server/Include/ServerDetector.py
+++ Server/Include/ServerDetector.py
@@ -1,51 +1,47 @@
 import tensorflow as tf
-import os
-from PIL import Image
 import numpy as np
-import cv2
-
-
-def convert_to_opencv(image):
-    # RGB -> BGR conversion is performed as well.
-    image = image.convert('RGB')
-    r,g,b = np.array(image).T
-    opencv_image = np.array([b,g,r]).transpose()
-    return opencv_image
+import cv2 as cv
 
 
 if __name__ == "__main__":
     imageFile = "D:\Git\ImageRecognition\Server\Images\Received.jpg"
+    model_filepath = "C:\Git\ImageRecognition\Server\Trained model\frozen_inference_graph.pb"
 
-    graph_def = tf.compat.v1.GraphDef()
-    labels = []
+with tf.gfile.FastGFile(model_filepath, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
 
-    # These are set to the default names from exported models, update as needed.
-    filename = 'C:\Git\ImageRecognition\Server\Trained model\frozen_inference_graph.pb'
-    labels_filename = 'C:\Git\ImageRecognition\Server\Trained model\label_map.pbtxt'
+with tf.Session() as sess:
+    # Restore session
+    sess.graph.as_default()
+    tf.import_graph_def(graph_def, name='')
 
-    # Import the TF graph
-    with tf.io.gfile.GFile(filename, 'rb') as f:
-        graph_def.ParseFromString(f.read())
-        tf.import_graph_def(graph_def, name='')
+    # Read and preprocess an image.
+    img = cv.imread(imageFile)
+    rows = img.shape[0]
+    cols = img.shape[1]
+    inp = cv.resize(img, (300, 300))
+    inp = inp[:, :, [2, 1, 0]]  # BGR2RGB
 
-    # Create a list of labels.
-    with open(labels_filename, 'rt') as lf:
-        for l in lf:
-            labels.append(l.strip())
+    # Run the model
+    out = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
+                    sess.graph.get_tensor_by_name('detection_scores:0'),
+                    sess.graph.get_tensor_by_name('detection_boxes:0'),
+                    sess.graph.get_tensor_by_name('detection_classes:0')],
+                   feed_dict={'image_tensor:0': inp.reshape(1, inp.shape[0], inp.shape[1], 3)})
 
-    image = Image.open(imageFile)
+    # Visualize detected bounding boxes.
+    num_detections = int(out[0][0])
+    for i in range(num_detections):
+        classId = int(out[3][0][i])
+        score = float(out[1][0][i])
+        bbox = [float(v) for v in out[2][0][i]]
+        if score > 0.3:
+            x = bbox[1] * cols
+            y = bbox[0] * rows
+            right = bbox[3] * cols
+            bottom = bbox[2] * rows
+            cv.rectangle(img, (int(x), int(y)), (int(right), int(bottom)), (125, 255, 51), thickness=2)
 
-    # Convert to OpenCV format
-    image = convert_to_opencv(image)
-
-    output_layer = 'loss:0'
-    input_node = 'Placeholder:0'
-
-    with tf.compat.v1.Session() as sess:
-        try:
-            prob_tensor = sess.graph.get_tensor_by_name(output_layer)
-            predictions, = sess.run(prob_tensor, {input_node: [image]})
-        except KeyError:
-            print("Couldn't find classification output layer: " + output_layer + ".")
-            print("Verify this a model exported from an Object Detection project.")
-            exit(-1)
+    cv.imshow('TensorFlow MobileNet-SSD', img)
+    cv.waitKey()
